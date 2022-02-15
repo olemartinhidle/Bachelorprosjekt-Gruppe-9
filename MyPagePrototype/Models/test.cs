26df3a99539589bb0ca7008a0975d3de6d87a102
Ole Martin Hidle <omhidle@uia.no>
2022-02-15 15:08:43 +0100

    Git test

---
+++ MyPagePrototype/Models/test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyPagePrototype.Models
+{
+    public class test
+    {
+        public int MyProperty { get; set; }
+
+        public test()
+        {
+            MyProperty = 1;
+
+        }
+    }
+
+
+}
