4cf37c3be2fcd0036672dce4167b8136effc44a7
Ole Martin Hidle <omhidle@uia.no>
2022-03-01 14:47:08 +0100

    Redirect

---
+++ MyPagePrototype/Controllers/MyCasesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPagePrototype.Controllers
+{
+    public class MyCasesController : Controller
+    {
+        // GET: MyCases
+        public ActionResult MyCases()
+        {
+            return View();
+        }
+    }
+}
